064d52a7cd097f129072bcae642b6c6856088ce4
suash <suash@workStation_ak>
2024-07-04 11:56:09 +0530

    Initial Commit

---
+++ src/main/java/com/swiggato/restaurant/RestaurantMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.swiggato.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiggato/restaurant/RestaurantMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggato.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
